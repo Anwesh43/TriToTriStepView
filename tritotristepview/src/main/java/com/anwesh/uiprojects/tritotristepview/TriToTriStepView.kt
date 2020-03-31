8db4ca3e47891780850b94f12a3c9ff4c5d73ecc
Anwesh43 <anweshthecool0@gmail.com>
2020-04-01 04:03:44 +0530

    adding TriToTriStepView class

--- tritotristepview/src/main/java/com/anwesh/uiprojects/tritotristepview/TriToTriStepView.kt
+++ tritotristepview/src/main/java/com/anwesh/uiprojects/tritotristepview/TriToTriStepView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawTTTNode(i : Int, scale : Float, paint : Paint) {
     drawTriToTriStep(scale, w, size, paint)
     restore()
 }
+
+class TriToTriStepView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
