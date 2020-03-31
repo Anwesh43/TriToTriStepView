abcf1465fffc84daca678252f4a21d3435b51fe3
Anwesh43 <anweshthecool0@gmail.com>
2020-04-01 03:48:30 +0530

    adding constants and libraries

--- tritotristepview/src/main/java/com/anwesh/uiprojects/tritotristepview/TriToTriStepView.kt
+++ tritotristepview/src/main/java/com/anwesh/uiprojects/tritotristepview/TriToTriStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.tritotristepview
 /**
  * Created by anweshmishra on 01/04/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+
+val nodes : Int = 5
+val tri : Int = 2
+val scGap : Float = 0.02f / (tri + 1)
+val delay : Long = 20
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
